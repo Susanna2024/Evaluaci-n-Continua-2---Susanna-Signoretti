a0d556a73835e6766e00a36c40dfc0e14c0f0fe6
Susanna2024 <[susanna.signoretti94@gmail.com]>
2024-04-09 15:10:06 +0200

    ADD RUTINA

---
+++ funzioni.js
@@ -0,0 +1,20 @@
+
+function hide() { 
+    document.getElementById("myP").style.visibility = "hidden"; 
+  } 
+  function hide2() { 
+    document.getElementById("myP2").style.visibility = "hidden"; 
+  }
+  function hide3() { 
+    document.getElementById("myP3").style.visibility = "hidden"; 
+  } 
+  function show() { 
+   document.getElementById("myP").style.visibility = "visible"; 
+  } 
+  function show2() { 
+   document.getElementById("myP2").style.visibility = "visible"; 
+  } 
+
+  function show3() { 
+   document.getElementById("myP3").style.visibility = "visible"; 
+  } 
