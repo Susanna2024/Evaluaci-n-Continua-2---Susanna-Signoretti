bb6cbfb1c6efd761df4594b382d774c94300ae50
Susanna2024 <[susanna.signoretti94@gmail.com]>
2024-04-13 20:28:56 +0200

    add faq

--- funzioni.js
+++ funzioni.js
@@ -19,40 +19,7 @@ function hide() {
    document.getElementById("myP3").style.visibility = "visible"; 
   } 
 
-    // video//
-
-
-  $(document).ready(function() {
-
-    // Gets the video src from the data-src on each button
-    
-    var $videoSrc;  
-    $('.video-btn').click(function() {
-        $videoSrc = $(this).data( "src" );
-    });
-    console.log($videoSrc);
-    
-      
-      
-    // when the modal is opened autoplay it  
-    $('#myModal').on('shown.bs.modal', function (e) {
-        
-    // set the video src to autoplay 
-    $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    
-    })
-      
-    
-    
-    // stop playing the youtube video when I close the modal
-    $('#myModal').on('hide.bs.modal', function (e) {
-
-//stoppit
-      $("#video").attr('src',$videoSrc); 
-    }) 
-        
-
-    });
+  
     
 
 
